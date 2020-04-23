44259ce86cb08fa08cad83a717e8e47c5adad4d1
Propene123 <benjamin.russell@durham.ac.uk>
2020-04-23 19:53:40 +0100

    lel

--- process_data.py
+++ process_data.py
@@ -6,17 +6,14 @@ ASSESSMENTS_FPATH = os.path.join(os.getcwd(), 'data', 'assessments.csv')
 COURSES_FPATH = os.path.join(os.getcwd(), 'data', 'courses.csv')
 STUDENT_ASSESS_FPATH = os.path.join(
     os.getcwd(), 'data', 'studentAssessment.csv')
-STUDENT_INFO_FPATH = os.path.join(os.getcwd(), 'data', 'studentAssessment.csv')
+STUDENT_INFO_FPATH = os.path.join(os.getcwd(), 'data', 'studentInfo.csv')
 STUDENT_REG_FPATH = os.path.join(
     os.getcwd(), 'data', 'studentRegistration.csv')
 STUDENT_VLE_FPATH = os.path.join(os.getcwd(), 'data', 'studentVle.csv')
 VLE_FPATH = os.path.join(os.getcwd(), 'data', 'vle.csv')
 
-def merge_data():
-    assessments = pd.read_csv(ASSESSMENTS_FPATH)
-    courses = pd.read_csv(COURSES_FPATH)
-    student_assess = pd.read_csv(STUDENT_ASSESS_FPATH)
-    student_info = pd.read_csv(STUDENT_INFO_FPATH)
-    student_reg = pd.read_csv(STUDENT_REG_FPATH)
-    student_vle = pd.read_csv(STUDENT_VLE_FPATH)
-    vle = pd.read_csv(VLE_FPATH)
+
+df = pd.read_csv(STUDENT_INFO_FPATH)
+print(df['id_student'].value_counts())
+fd = pd.read_csv(STUDENT_ASSESS_FPATH)
+print(fd['id_student'].value_counts())
